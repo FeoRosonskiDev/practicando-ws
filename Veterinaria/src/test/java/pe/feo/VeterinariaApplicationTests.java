dc8345f1d746291f17caa4ba5d52a230939ed73c
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2019-07-18 00:39:34 -0500

    proyecto spring pom.xml configurado

---
+++ Veterinaria/src/test/java/pe/feo/VeterinariaApplicationTests.java
@@ -0,0 +1,16 @@
+package pe.feo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VeterinariaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
